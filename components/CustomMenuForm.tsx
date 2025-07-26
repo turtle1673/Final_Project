391c0244e9f77f46a0f290bb0462a3ef8b20d81c
nickykung123 <Email>
2025-07-26 21:52:56 +0700

    updateNow

---
+++ components/CustomMenuForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function custommenu() {
+  return (
+    <section className="text-4xl text-white flex items-center justify-center min-h-screen px-4 sm:px-8 md:px-16 lg:px-32">
+      <div>Custom Menu</div>
+    </section>
+  );
+}
---
+++ components/MyorderForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function myorder() {
+  return (
+    <section className="text-4xl text-white flex items-center justify-center min-h-screen px-4 sm:px-8 md:px-16 lg:px-32">
+      <div>My Order</div>
+    </section>
+  );
+}
