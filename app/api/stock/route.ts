d0fea90151022135d52170d0a1cfdf1b307f9e37
turtle1673 <66026189@up.ac.th>
2025-08-05 22:31:02 +0700

    http method สำหรับ stock

--- app/api/stock/route.ts
+++ app/api/stock/route.ts
@@ -1,9 +1,24 @@
+import prisma from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export default function GET(_req: Request) {
-  const data = {
-    name: "coffee",
-    price: 100
+export async function GET(_req: Request) {
+  const stocks = await prisma.stockItem.findMany()
+  return new NextResponse(JSON.stringify(stocks), { status: 200 })
+}
+
+export async function POST(req: Request) {
+  const body = await req.json()
+  try {
+  const item = await prisma.stockItem.create({
+    data: body
+  })
+  return new NextResponse(JSON.stringify({message : "item created",item}), { status: 201 })
+}
+  catch (error: any) {
+    console.error("Error creating stock item:", error)
+    return new NextResponse(
+      JSON.stringify({ error: error.message || "An unexpected error occurred" }),
+      { status: 500 }
+    )
   }
-  return new NextResponse(JSON.stringify(data), { status: 200 });
 }
